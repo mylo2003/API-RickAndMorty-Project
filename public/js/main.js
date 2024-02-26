ae92e8dbb241f456b8d0a1fa61aab831492aae68
Camilo Mora <camiloalf23@gmail.com>
2024-02-26 18:27:23 -0500

    Mejoras visuales v3, corregimos comportamiento del codigo en js

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,22 @@
 console.log('Wubba Lubba Dub Dub!!!');
 
-const URL = 'https://rickandmortyapi.com/api'; 
+const API = 'https://rickandmortyapi.com/api'; 
 
+const getCharacters = async () => {
+  let randomPage = Math.floor(Math.random() * 43);
 
+  const response = await fetch(`${API}/character/?page=${randomPage}`);
+  const data = await response.json();
 
-
-/*
-function getRandomCharacter (done) {
-
-  const results = fetch(`${URL}/character/`);
-
-  results 
-    .then(response => response.json())
-    .then(data => {
-      done(data);
-    });
-}
-
-getRandomCharacter (data => {
-
-  console.log(data);
-});
-*/
-
-
-
-
-
-async function getCharacters() {
-  const response = await fetch(`${URL}/character/`);
-  let data = await response.json();
-  const amount = data.info.pages;
-
-  console.log(data.info.next);
-  
   let randomNum = Math.floor(Math.random() * data.results.length);
   let randomNum2 = Math.floor(Math.random() * data.results.length);
   let randomNum3 = Math.floor(Math.random() * data.results.length);
+
+  while(randomNum == randomNum2 || randomNum == randomNum3 || randomNum2 == randomNum3) {
+    randomNum = Math.floor(Math.random() * data.results.length);
+    randomNum2 = Math.floor(Math.random() * data.results.length);
+    randomNum3 = Math.floor(Math.random() * data.results.length);
+  }
 
   const img = document.getElementById('img-random');
   const img2 = document.getElementById('img-random2');
@@ -64,32 +44,10 @@ async function getCharacters() {
   img3.alt = data.results[randomNum3].name;
   name3.innerText = data.results[randomNum3].name;
   status3.innerText = data.results[randomNum].status;
-  
-
-  const btn = document.querySelector('button');
-
-  btn.addEventListener('click', ()=> {
-    
-    randomNum = Math.floor(Math.random() * data.results.length);
-    randomNum2 = Math.floor(Math.random() * data.results.length);
-    randomNum3 = Math.floor(Math.random() * data.results.length);
-
-    img.src = data.results[randomNum].image;
-    img.alt = data.results[randomNum].name;
-    name.innerText = data.results[randomNum].name;
-    status.innerText = data.results[randomNum].status;
-  
-    img2.src = data.results[randomNum2].image;
-    img2.alt = data.results[randomNum2].name;
-    name2.innerText = data.results[randomNum2].name;
-    status2.innerText = data.results[randomNum].status;
-  
-    img3.src = data.results[randomNum3].image;
-    img3.alt = data.results[randomNum3].name;
-    name3.innerText = data.results[randomNum3].name;
-    status3.innerText = data.results[randomNum].status;
-
-  });
-} 
+}
 
 getCharacters();
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getCharacters);
